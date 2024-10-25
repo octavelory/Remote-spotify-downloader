3dfdbc6a222f37c35ffdd4f02f1752b22197ffc0
Octave <octave.lory@gmail.com>
2024-10-25 21:09:58 +0200

    init

--- modal_deployment.py
+++ modal_deployment.py
@@ -1,11 +1,13 @@
 import modal
-import yank_module
+import Remote_spotify_downloader.yank_module as yank_module
 import asyncio
 
 image = (
     modal.Image.debian_slim(python_version="3.10")
     .apt_install("git")
-    .run_commands("git clone")
+    .run_commands("git clone https://github.com/octavelory/Remote-spotify-downloader")
+    .run_commands("mv Remote-spotify-downloader Remote_spotify_downloader")
+    .run_commands("pip install -r Remote-spotify-downloader/requirements.txt")
 )
 
 app = modal.App(name="spotify-downloader")
