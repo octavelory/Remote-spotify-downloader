46a3300fccb8a7e5a7cca97b0cfece44f53b4900
Octave <octave.lory@gmail.com>
2024-10-26 12:16:32 +0200

    init

--- app/util/download.py
+++ app/util/download.py
@@ -8,8 +8,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 from pydeezer import Deezer, Downloader
 from pydeezer.constants import track_formats
-from util.spotify import spotify_isrc, spotify_playlist
-from util.deezer import get_deezer_track
+from spotify import spotify_isrc, spotify_playlist
+from deezer import get_deezer_track
 from cryptography.utils import CryptographyDeprecationWarning
 
 warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
